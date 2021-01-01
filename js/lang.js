c97160252e44cf6ca023e67efeffee54d11c45df
malain96 <alain.mathieu1996@gmail.com>
2021-01-01 15:53:12 +0100

    Updated website and removed contact form

--- js/lang.js
+++ js/lang.js
@@ -2,7 +2,7 @@ var language;
 function getLanguage() {
     (localStorage.getItem('language') == null) ? setLanguage('en') : false;
     $.ajax({
-        url:  'lang/' +  localStorage.getItem('language') + '.json',
+        url: 'lang/' + localStorage.getItem('language') + '.json',
         dataType: 'json', async: false, dataType: 'json',
         success: function (lang) {
             language = lang
@@ -19,7 +19,7 @@ function setLanguage(lang) {
     loadText();
 }
 
-function loadText (){
+function loadText() {
     $('#title').text(language.title);
     $('#menuTitle').text(language.menuTitle);
     $('#aboutMenu').text(language.aboutMenu);
@@ -38,23 +38,16 @@ function loadText (){
     $('#websiteBody').text(language.websiteBody);
     $('#dbSaverTitle').text(language.dbSaverTitle);
     $('#dbSaverBody').text(language.dbSaverBody);
+    $('#hlamnikTitle').text(language.hlamnikTitle);
+    $('#hlamnikBody').text(language.hlamnikBody);
     $('#contactTitle').text(language.contactTitle);
     $('#contactIntro').html(language.contactIntro);
-    $('#name').attr('placeholder', language.placeholderName)
-        .attr('data-validation-required-message', language.nameRequired);
-    $('#email').attr('placeholder', language.placeholderEmail)
-        .attr('data-validation-required-message', language.emailRequired)
-        .attr('data-validation-email-message', language.emailValid);
-    $('#phone').attr('placeholder', language.placeholderPhone);
-    $('#message').attr('placeholder', language.placeholderMessage)
-        .attr('data-validation-required-message', language.messageRequired);
-    $('#sendMessageButton').text(language.sendMessage);
-    $('#resumeLink').attr('href',language.resumeLink);
-    $('#resumeIcon').attr('title',language.altResume);
+    $('#resumeLink').attr('href', language.resumeLink);
+    $('#resumeIcon').attr('title', language.altResume);
     $('.age').text(getAge('1996/05/11'));
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
     getLanguage();
     loadText();
 });
